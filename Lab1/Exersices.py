9263659fd66dc7da8b7fb993dbb5cc3ec1ff1ef1
daniilshevch <daniil.shevchenko.shi.2023@lpnu.ua>
2025-09-17 20:37:36 +0300

    17.09 20:37

--- Lab1/Exersices.py
+++ Lab1/Exersices.py
@@ -107,3 +107,119 @@ a = np.array([1,2,3,4,5])
 b = np.array([5,6,7,8,9])
 out = np.setdiff1d(a,b)
 print(out)
+#7
+print("#7")
+a = np.array([1,2,3,2,3,4,3,4,5,6])
+b = np.array([7,2,10,2,7,4,9,4,9,8])
+mask = (a == b)
+print(mask)
+res = np.where(mask)[0]
+print(res)
+#8
+print("#8")
+a = np.array([2, 6, 1, 9, 10, 3, 27])
+mask = (a >= 5) & (a <= 10)
+indexes = np.where((a >= 5) & (a <= 10))
+print(indexes)
+print(a[indexes])
+print(a[mask])
+#9
+print("#9")
+def maxx(x, y):
+    """Get the maximum of two items"""
+    return x if x >= y else y
+
+
+a = np.array([5, 7, 9, 8, 6, 4, 5])
+b = np.array([6, 3, 4, 8, 9, 7, 1])
+maxx_vect = np.vectorize(maxx)
+print(maxx_vect(a,b))
+#10
+print("#10")
+arr = np.array([[0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8]])
+arr2 = np.array([[0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8]])
+arr[:, 0:2] = arr[:, 1::-1]
+#arr2[:, [0,1]] = arr2[:, [1,0]]
+arr2 = arr2[:,[1,0,2]]
+print(arr)
+print(arr2)
+#11
+print("#11")
+arr = np.array([[3, 4, 5],
+                [0, 1, 2],
+                [6, 7, 8]])
+arr2 = np.array([[3, 4, 5],
+                [0, 1, 2],
+                [6, 7, 8]])
+arr[0:2, :] = arr[1::-1, :]
+arr2 = arr2[[1, 0, 2],:]
+print(arr)
+print(arr2)
+#12
+print("#12")
+arr = np.array([[3, 4, 5],
+                [0, 1, 2],
+                [6, 7, 8]])
+arr[::,:] = arr[::-1, :]
+print(arr)
+#13
+print("#13")
+arr = np.array([[3, 4, 5],
+                [0, 1, 2],
+                [6, 7, 8]])
+arr2 = np.array([[3, 4, 5],
+                [0, 1, 2],
+                [6, 7, 8]])
+out = arr2[:, ::-1]
+arr[:, ::] = arr[:, ::-1]
+print(arr)
+print(out)
+#14
+print("#14")
+arr = np.random.uniform(5, 10, size = (5,3))
+arr2 = np.random.randint(low=5, high=10, size=(5,3)) + np.random.random((5,3))
+print(arr)
+print(arr2)
+#15
+print("#15")
+arr = np.array([[1,2,3,4,5],
+                [1,2,3,4,5],
+                [1,2,3,4,5],
+                [1,2,3,4,5]])
+extract_column_index = 4
+column = arr[:, extract_column_index]
+print(column)
+#16
+print("#16")
+arr = np.array([(1,2,3,4,5),
+                (1,2,3,4,5),
+                (1,2,3,4,5),
+                (1,2,3,4,5)])
+extract_column_index = 4
+res = arr[:, extract_column_index]
+res2 = np.array([row[extract_column_index] for row in arr])
+print(res)
+print(res2)
+#17
+print("#17")
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+min = arr.min()
+max = arr.max()
+arr_norm = (arr - min) / (max - min)
+print(arr_norm)
+#18
+print("#18")
+matr = np.array([[1.,2.,3.],
+                 [1.,2.,3.],
+                 [1.,2.,3.]])
+
+first_indexes = np.random.randint(low = 0, high = 3, size = 2)
+second_indexes = np.random.randint(low = 0, high = 3, size = 2)
+matr[first_indexes, second_indexes] = np.nan
+print(matr)
+#19
+
